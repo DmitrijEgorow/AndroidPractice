35c8b7779a3c650717869c3ac7b97b833dbceba4
Dmitry Egorov <tegorkrsk@gmail.com>
2022-04-07 19:29:35 +0300

    grayscale

--- app/src/androidTest/java/ru/myitschool/lab23/InstrumentedTest.kt
+++ app/src/androidTest/java/ru/myitschool/lab23/InstrumentedTest.kt
@@ -1,27 +1,26 @@
 package ru.myitschool.lab23
 
-import android.Manifest
 import android.app.Instrumentation
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.content.res.Configuration
 import android.os.Bundle
-import android.os.LocaleList
 import android.util.Log
 import android.view.View
-import androidx.annotation.CallSuper
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.FailureHandler
+import androidx.test.espresso.assertion.PositionAssertions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.base.DefaultFailureHandler
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.UiDevice
+import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.Matcher
 import org.junit.*
 import org.junit.runner.RunWith
@@ -31,6 +30,7 @@ import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SmallTest
 class InstrumentedTest {
     //add/remove seed
     private val random = Random()
@@ -43,6 +43,8 @@ class InstrumentedTest {
 
     // https://github.com/rainbowcake/rainbowcake
 
+    // https://stackoverflow.com/questions/40508113/android-espresso-testing-close-and-app-and-then-re-open-it
+
 
     private val n = 1 // number of iterations in each test
 
@@ -52,8 +54,6 @@ class InstrumentedTest {
         mInstrumentation = InstrumentationRegistry.getInstrumentation()
         handler = DescriptionFailureHandler(mInstrumentation)
         Espresso.setFailureHandler(handler)
-
-
 
 
         val nonLocalizedContext = mInstrumentation.targetContext
@@ -66,31 +66,24 @@ class InstrumentedTest {
         val intent = Intent(appContext, MainActivity::class.java)
         activityScenario = ActivityScenario.launch(intent)
 
-        Log.d("lang",  appContext.resources.getString(
-            appContext.resources.getIdentifier(
-                "main_text",
-                "string",
-                appContext.opPackageName
+        Log.d(
+            "lang", appContext.resources.getString(
+                appContext.resources.getIdentifier(
+                    "main_text",
+                    "string",
+                    appContext.opPackageName
+                )
             )
-        ))
+        )
 
-        editTextGithubId = appContext.resources
-            .getIdentifier("edit_text_github_login", "id", appContext.opPackageName)
-        buttonSendId = appContext.resources
-            .getIdentifier("button_send", "id", appContext.opPackageName)
-        loginId = appContext.resources
-            .getIdentifier("requested_login", "id", appContext.opPackageName)
-        yearId = appContext.resources
-            .getIdentifier("requested_year", "id", appContext.opPackageName)
-        twitterId = appContext.resources
-            .getIdentifier("requested_twitter", "id", appContext.opPackageName)
+        mainTextId = appContext.resources
+            .getIdentifier("main_text", "id", appContext.opPackageName)
 
 
         for ((i, e) in colors.withIndex()) {
             rainbowIds[i] = appContext.resources
                 .getIdentifier(e, "id", appContext.opPackageName)
         }
-
     }
 
     private fun checkInterface(ids: IntArray) {
@@ -106,19 +99,30 @@ class InstrumentedTest {
         //Check existence of views
         addTestToStat(1)
         checkInterface(
-            intArrayOf(
-                editTextGithubId, buttonSendId, loginId,
-                yearId, twitterId
+            rainbowIds
+        )
+        onView(withId(mainTextId))
+            .check(matches(isDisplayed()))
+
+        /*onView(
+            allOf(
+                withId(mainTextId),
+                withText(MAIN_TEXT)
             )
         )
-        onView(withId(editTextGithubId))
-            .check(matches(isDisplayed()))
-        onView(withId(buttonSendId))
-            .check(matches(isDisplayed()))
+            .check(matches(isDisplayed()))*/
 
         rotateDevice(true)
 
-        //todo add checks
+        /*onView(
+            allOf(
+                withId(mainTextId),
+                withText(MAIN_TEXT)
+            )
+        )
+            .check(matches(isDisplayed()))*/
+
+        //todo add checks for landscape mode
 
         rotateDevice(false)
 
@@ -126,12 +130,12 @@ class InstrumentedTest {
     }
 
     @Test
-    fun checkResPortrait() {
+    fun checkResValues() {
         //Check string resource
         addTestToStat(1)
 
 
-        Thread.sleep(7_000)
+        Thread.sleep(THREAD_DELAY)
 
         /*onView(withId(
 
@@ -140,16 +144,7 @@ class InstrumentedTest {
         ))
             .check(matches(withText("Основной текст")))*/
 
-        Assert.assertNotEquals(
-            "Mismatch exception: 'app_name' resource does not exist",
-            0,
-            appContext.resources.getIdentifier(
-                "app_name",
-                "string",
-                appContext.opPackageName
-            ).toLong()
-        )
-
+        //todo remove
         Assert.assertNotEquals(
             "Mismatch exception: 'vertical' resource does not exist",
             0,
@@ -185,6 +180,41 @@ class InstrumentedTest {
                 )
             }
         }
+
+
+        addTestToPass(1)
+    }
+
+    @Test
+    fun checkResPortait() {
+        addTestToStat(1)
+
+        checkInterface(
+            rainbowIds
+        )
+
+        for ((i, e) in colors.withIndex()) {
+            onView(withId(rainbowIds[i]))
+                .check(matches(isDisplayed()))
+            if (i + 1 < colors.size) {
+                // onView(withId(rainbowIds[i+1]))
+                //    .check(matches(isDisplayed()))
+                onView(withId(rainbowIds[i + 1]))
+                    .check(
+                        isCompletelyBelow(
+                            withId(rainbowIds[i])
+                        )
+                    )
+            }
+        }
+
+        onView(withId(mainTextId))
+            .check(
+                isCompletelyAbove(
+                    withId(rainbowIds[0])
+                )
+            )
+
 
 
         addTestToPass(1)
@@ -196,57 +226,37 @@ class InstrumentedTest {
         //Check string resource
         addTestToStat(1)
 
+        checkInterface(
+            rainbowIds
+        )
 
         rotateDevice(true)
-        Thread.sleep(7_000)
+        Thread.sleep(THREAD_DELAY)
 
-        Assert.assertNotEquals(
-            "Mismatch exception: 'app_name' resource does not exist",
-            0,
-            appContext.resources.getIdentifier(
-                "app_name",
-                "string",
-                appContext.opPackageName
-            ).toLong()
-        )
-
-        Assert.assertNotEquals(
-            "Mismatch exception: 'vertical' resource does not exist",
-            0,
-            appContext.resources.getIdentifier(
-                "vertical",
-                "string",
-                appContext.opPackageName
-            ).toLong()
-        )
-
-        Assert.assertNotEquals(
-            "Mismatch exception: 'horizontal' resource does not exist",
-            0,
-            appContext.resources.getIdentifier(
-                "horizontal",
-                "string",
-                appContext.opPackageName
-            ).toLong()
-        )
-
-
-        val colorResIds = arrayOf(0, 0, 0, 0, 0, 0, 0)
         for ((i, e) in colors.withIndex()) {
-            colorResIds[i] = appContext.resources
-                .getIdentifier(e, "colors", appContext.opPackageName)
-        }
-        if (colorResIds.reduce(Int::times) != 0) {
-            for ((i, e) in colors.withIndex()) {
-                Assert.assertEquals(
-                    "Mismatch exception: $e as a color does not match the one in the task",
-                    appContext.getColor(colorResIds[i]),
-                    colorsCorrectValues[i]
-                )
+            onView(withId(rainbowIds[i]))
+                .check(matches(isDisplayed()))
+            if (i + 1 < colors.size) {
+                // onView(withId(rainbowIds[i+1]))
+                //    .check(matches(isDisplayed()))
+                onView(withId(rainbowIds[i + 1]))
+                    .check(
+                        isCompletelyRightOf(
+                            withId(rainbowIds[i])
+                        )
+                    )
             }
         }
 
+        onView(withId(mainTextId))
+            .check(
+                isCompletelyAbove(
+                    withId(rainbowIds[0])
+                )
+            )
+
         rotateDevice(false)
+
         addTestToPass(1)
     }
 
@@ -277,17 +287,18 @@ class InstrumentedTest {
 
     companion object {
         private const val APP_NAME = "Lab23"
-        private const val THREAD_DELAY: Long = 4_700
-        private const val BUTTON_SEND_TEXT = "Send Request"
+        private const val THREAD_DELAY: Long = 300
+        private const val MAIN_TEXT = "Основной текст"
         private const val EMPTY_STRING = ""
-        private const val TEXT = "Richard (red) Of (orange) York (yellow) Gave (green) Battle (blue) In (indigo) Vain (violet)"
+        private const val TEXT =
+            "Richard (red) Of (orange) York (yellow) Gave (green) Battle (blue) In (indigo) Vain (violet)"
 
         private var grade = 0
         private var totalTests = 0
         private var maxGrade = 0
         private var passTests = 0
 
-        private var editTextGithubId = 0
+        private var mainTextId = 0
         private var buttonSendId = 0
         private var loginId = 0
         private var yearId = 0
