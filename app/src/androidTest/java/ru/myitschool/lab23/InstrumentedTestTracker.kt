b2d061c4f523a845019b0a4ecab02e12cfca6db5
myitschool <tegorkrsk@gmail.com>
2022-11-23 21:41:04 +0300

    lab29 present full project

--- app/src/androidTest/java/ru/myitschool/lab23/InstrumentedTestTracker.kt
+++ app/src/androidTest/java/ru/myitschool/lab23/InstrumentedTestTracker.kt
@@ -16,7 +16,9 @@ import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import io.github.kakaocup.kakao.edit.KEditText
+import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.screen.Screen
@@ -38,22 +40,6 @@ import kotlin.math.min
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @MediumTest
 class InstrumentedTestTracker {
-    //add/remove seed
-    private val random = Random()
-    private var outFlag = false
-    private var lastNumber = 0.0
-
-    private val limit = 13
-    private var index = 0
-    private var count = 0
-    private var countNegative = 0
-
-    private val inputValue = 1000
-    private val inputDoubleText = "1000.0"
-    private val inputText = "1000"
-
-    private val income = "Income"
-    private val expenses = "Expenses"
 
     private var activityScenario: ActivityScenario<MainActivity>? = null
     private var handler: DescriptionFailureHandler? = null
@@ -76,39 +62,14 @@ class InstrumentedTestTracker {
         appContext = nonLocalizedContext.createConfigurationContext(configuration)
 
         val intent = Intent(appContext, MainActivity::class.java)
-        //intent.putExtra("long number", inputNumbers[index])
-        Log.d("Tests", "index = $index")
 
         activityScenario = ActivityScenario.launch(intent)
 
-        efAmountCardId =
-            appContext.resources.getIdentifier("ef_amount_card", "id", appContext.opPackageName)
-        efCurrentBalanceTextId = appContext.resources.getIdentifier(
-            "ef_current_balance_text",
-            "id",
-            appContext.opPackageName
-        )
-        efExpensesRvId =
-            appContext.resources.getIdentifier("ef_expenses_rv", "id", appContext.opPackageName)
-        expenseTypeTextId =
-            appContext.resources.getIdentifier("expense_type_text", "id", appContext.opPackageName)
-        expenseDateTextId =
-            appContext.resources.getIdentifier("expense_date_text", "id", appContext.opPackageName)
-        expenseAmountTextId = appContext.resources.getIdentifier(
-            "expense_amount_text",
-            "id",
-            appContext.opPackageName
-        )
-        typeSpinnerId =
-            appContext.resources.getIdentifier("type_spinner", "id", appContext.opPackageName)
-        expenseAmountEditTextId = appContext.resources.getIdentifier(
-            "expense_amount_edit_text",
-            "id",
-            appContext.opPackageName
-        )
-        addButtonId =
-            appContext.resources.getIdentifier("add_button", "id", appContext.opPackageName)
-        addFabId = appContext.resources.getIdentifier("add_fab", "id", appContext.opPackageName)
+        imageId =
+            appContext.resources.getIdentifier("box_image", "id", appContext.opPackageName)
+        buttonId =
+            appContext.resources.getIdentifier("test_button", "id", appContext.opPackageName)
+
 
     }
 
@@ -127,224 +88,85 @@ class InstrumentedTestTracker {
 
 
     @Test(timeout = MAX_TIMEOUT)
-    fun mainTest() {
-        //
-        addTestToStat(limit + 3)
+    fun aPortraitOrientationTest() {
+        addTestToStat(1)
         checkInterface(
             intArrayOf(
-                efAmountCardId, efCurrentBalanceTextId, efExpensesRvId, addFabId
+                imageId, buttonId
             ), CHECK_INTERFACE_MESSAGE
         )
-
-        /*run {
-            mainTestCheckStep()
-            addTestToPass(1)
-        }*/
-        mainTestCheckStep()
-        //addTestToPass(1)
+        checkStep(false)
+        addTestToPass(1)
     }
 
-    private fun mainTestCheckStep() {
-        class Item(parent: Matcher<View>) : KRecyclerItem<Double>(parent) {
-            val type = KEditText(parent) { withId(expenseTypeTextId) }
+    @Test(timeout = MAX_TIMEOUT)
+    fun bLandscapeOrientationTest() {
+        //
+        addTestToStat(1)
+        checkInterface(
+            intArrayOf(
+                imageId, buttonId
+            ), CHECK_INTERFACE_MESSAGE
+        )
+        checkStep(true)
+        addTestToPass(1)
+    }
 
-            //todo
-            val date = KEditText(parent) { withId(expenseDateTextId) }
-            val amount = KEditText(parent) { withId(expenseAmountTextId) }
-        }
+
+    @Test(timeout = MAX_TIMEOUT)
+    fun cStringTest() {
+        addTestToStat(1)
+        Assert.assertNotEquals(
+            "Mismatch exception: string resource does not exist",
+            0,
+            appContext.resources.getIdentifier(
+                "main_text_button",
+                "string",
+                appContext.opPackageName
+            ).toLong()
+        )
+        addTestToPass(1)
+    }
+
+    private fun checkStep(needsRotate : Boolean) {
 
         class SearchScreen : Screen<SearchScreen>() {
-            val efAmountCard = KTextView { withId(efAmountCardId) }
-            val efCurrentBalanceText = KTextView { withId(efCurrentBalanceTextId) }
-            val addFab = KButton { withId(addFabId) }
-
-            val typeSpinner = KSpinner(builder = { withId(typeSpinnerId) },
-                itemTypeBuilder = { itemType(::KSpinnerItem) })
-            val expenseAmountEditText = KEditText { withId(expenseAmountEditTextId) }
-            val addButton = KButton { withId(addButtonId) }
-
-            val recyclerView = KRecyclerView(builder = { withId(efExpensesRvId) },
-                itemTypeBuilder = { itemType(::Item) })
+            val button = KButton { withId(buttonId) }
+            val image = KImageView { withId(imageId) }
         }
 
 
         val screen = SearchScreen()
         screen {
+            val orange =
+                appContext.resources
+                    .getIdentifier("orange", "drawable", appContext.opPackageName)
+            val blue =
+                appContext.resources
+                    .getIdentifier("blue", "drawable", appContext.opPackageName)
 
-            fun inputIterations(isExpensesType: Boolean = true) {
-                for (i in 1 until limit + 1) {
-                    addFab.click()
+            image.matches { isDisplayed() }
+            button.matches { isDisplayed() }
 
-                    typeSpinner {
-                        isVisible()
-                        hasSize(2)
-                        open()
-                        emptyFirstChild {
-                            isVisible()
-                            hasText(income)
-                        }
-                        emptyLastChild {
-                            isVisible()
-                            hasText(expenses)
-                            click()
-                        }
-                        hasText(expenses)
-                    }
-                    expenseAmountEditText.typeText(inputText)
-                    count++
-                    closeSoftKeyboard()
+            image.isCompletelyAbove { withId(buttonId) }
+            button.isCompletelyBelow { withId(imageId) }
+            image.hasDrawable(orange, null)
 
-                    addButton.click()
+            // checks ui after rotation
+            if (needsRotate) {
+                rotateDevice(true)
+                Thread.sleep(THREAD_DELAY)
 
-                    addTestToPass(1) // depends on limit
+                image.matches { isDisplayed() }
+                button.matches { isDisplayed() }
+                image.isCompletelyLeftOf { withId(buttonId) }
+                button.isCompletelyRightOf { withId(imageId) }
 
-                    Log.d("Tests", "${recyclerView.getSize()}")
-                    assertEquals(
-                        "List has inappropriate number of elements", count, recyclerView.getSize()
-                    )
-                    Log.d("Tests", "$i")
-                    val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.UK)
-                    val date: String = sdf.format(System.currentTimeMillis())
-                    recyclerView {
-                        childAt<Item>(count - 1) {
-                            amount {
-                                isVisible()
-                                hasText(inputDoubleText)
-                            }
-                            date {
-                                isVisible()
-                                hasText(date)
-                            }
-                            type {
-                                isVisible()
-                                hasText(expenses)
-                            }
-                        }
-                    }
-                    efCurrentBalanceText.hasText("-${inputValue * count}.0")
+                image.hasDrawable(blue, null)
 
-                    if ((i % 2 == 0) and (count > 0)) {
-                        handler?.extraMessage =
-                            "Attempt to delete an element with index ${count - 1}, len = ${recyclerView.getSize()}"
-                        recyclerView {
-                            childAt<Item>(count - 1) {
-                                amount {
-                                    longClick()
-                                }
-                            }
-                        }
-                        uiDevice?.findObject(By.textStartsWith("Delete"))?.click()
-                        count--
-                        Thread.sleep(THREAD_DELAY)
-                        efCurrentBalanceText.hasText("-${inputValue * count}.0")
-                        handler?.extraMessage = ""
-                    }
-
-                    if ((i % 3 == 0) and (count > 0)) {
-                        handler?.extraMessage =
-                            "Attempt to duplicate an element with index ${count - 1}, len = ${recyclerView.getSize()}"
-                        recyclerView {
-                            childAt<Item>(count - 1) {
-                                amount {
-                                    longClick()
-                                }
-                            }
-                        }
-                        uiDevice?.findObject(By.textStartsWith("Duplicate"))?.click()
-                        count++
-                        Thread.sleep(THREAD_DELAY)
-                        efCurrentBalanceText.hasText("-${inputValue * count}.0")
-                        handler?.extraMessage = ""
-                    }
-
-                    if ((i % 5 == 0) and (count > 0)) {
-                        handler?.extraMessage =
-                            "Attempt to delete an element with index 0, len = ${recyclerView.getSize()}"
-                        recyclerView {
-                            childAt<Item>(0) {
-                                amount {
-                                    longClick()
-                                }
-                            }
-                        }
-                        uiDevice?.findObject(By.textStartsWith("Delete"))?.click()
-                        count--
-                        Thread.sleep(THREAD_DELAY)
-                        efCurrentBalanceText.hasText("-${inputValue * count}.0")
-                        handler?.extraMessage = ""
-                    }
-                }
+                rotateDevice(false)
             }
 
-            inputIterations()
-
-            countNegative = count
-            for (i in 0 until 2) {
-                addFab.click()
-
-                typeSpinner {
-                    isVisible()
-                    hasSize(2)
-                    open()
-                    emptyLastChild {
-                        isVisible()
-                        hasText(expenses)
-                    }
-                    emptyFirstChild {
-                        isVisible()
-                        hasText(income)
-                        click()
-                    }
-                    hasText(income)
-                }
-                expenseAmountEditText.typeText("${(i + 1) * 2 * inputValue}")
-                count++
-                closeSoftKeyboard()
-
-                addButton.click()
-
-                Log.d("Tests", "${recyclerView.getSize()}")
-                assertEquals(
-                    "List has inappropriate number of elements", count, recyclerView.getSize()
-                )
-                Log.d("Tests", "$i")
-                val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.UK)
-                val date: String = sdf.format(System.currentTimeMillis())
-                recyclerView {
-                    scrollTo(count - 1)
-                    lastChild<Item> {
-                        amount {
-                            isVisible()
-                            hasText("${(i + 1) * 2 * inputValue}.0")
-                        }
-                        date {
-                            isVisible()
-                            hasText(date)
-                        }
-                        type {
-                            isVisible()
-                            hasText(income)
-                        }
-                    }
-                }
-                if (i == 0) {
-                    efCurrentBalanceText.hasText("${-inputValue * countNegative + 2 * inputValue}.0")
-                } else {
-                    efCurrentBalanceText.hasText("${-inputValue * countNegative + 2 * inputValue + 2 * 2 * inputValue}.0")
-                }
-                addTestToPass(1)
-            }
-
-            // checks saving state after rotation
-            rotateDevice(true)
-            handler?.extraMessage = "Rotating device"
-            Thread.sleep(THREAD_DELAY)
-            assertEquals(
-                "RecyclerView has inappropriate number of elements", count, recyclerView.getSize()
-            )
-            addTestToPass(1)
-            rotateDevice(false)
-            handler?.extraMessage = ""
 
         }
     }
@@ -379,23 +201,15 @@ class InstrumentedTestTracker {
         private const val APP_NAME = "Context Menu"
         private const val CHECK_INTERFACE_MESSAGE = "Some UI elements seems to be missing"
         private const val THREAD_DELAY: Long = 1_500
-        private const val MAX_TIMEOUT: Long = 3 * 60_000 // 50sec
+        private const val MAX_TIMEOUT: Long = 10_000 // 20sec
 
         private var grade = 0
         private var totalTests = 1
         private var maxGrade = 0
         private var passTests = 0
 
-        private var efAmountCardId = 0
-        private var efCurrentBalanceTextId = 0
-        private var efExpensesRvId = 0
-        private var expenseTypeTextId = 0
-        private var expenseDateTextId = 0
-        private var expenseAmountTextId = 0
-        private var typeSpinnerId = 0
-        private var expenseAmountEditTextId = 0
-        private var addButtonId = 0
-        private var addFabId = 0
+        private var buttonId = 0
+        private var imageId = 0
 
         @BeforeClass
         @JvmStatic
@@ -436,7 +250,7 @@ class DescriptionFailureHandler(instrumentation: Instrumentation) : FailureHandl
 
                 extraMessage + "     " + error.message?.substring(
                     0, min( //todo change length
-                        100000, error.message?.length ?: 0
+                        130, error.message?.length ?: 0
                     )
                 ) + "...", error.cause
             )
