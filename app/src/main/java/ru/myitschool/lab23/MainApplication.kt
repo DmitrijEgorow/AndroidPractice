1a8776d8c6e0f4b7a1f47867868b3ad8d3bea845
Dmitry <tegorkrsk@gmail.com>
2024-05-21 14:44:49 +0300

    lab63 vickrey auction student project

---
+++ app/src/main/java/ru/myitschool/lab23/MainApplication.kt
@@ -0,0 +1,11 @@
+package ru.myitschool.lab23
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // do not put anything here
+    }
+}
